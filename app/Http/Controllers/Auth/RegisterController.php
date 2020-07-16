50a2479f42ffea2d969c8227fa8ff7ae6ae889f6
Abdul Raof <abdulraof628@gmail.com>
2020-07-16 14:08:59 +0800

    'Updates

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,14 +67,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'role' => $data['role'],
         ]); 
 
-        return RoleUser::create([
+        RoleUser::create([
             'role_id' => $data['role'],
             'user_id' => $user->id,
         ]);
